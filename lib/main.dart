d0a7a010c919d45c5697a19b43cf536aa35df746
abhinichi <abhijit.behera@nichi.com>
2025-12-17 17:58:14 +0530

    final changes wrt 3d model with colours and shades for mug cup inside area

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,9 @@
 
 import 'dart:math' as Math;
 
-import 'package:dummy_model/htmlThreeDModel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_3d_controller/flutter_3d_controller.dart';
-
-import 'flu_3d_controller.dart';
 import 'flutter_cube.dart';
-import 'model_viewer_plus.dart';
 
 void main() {
   runApp(MyApp());
@@ -60,7 +56,7 @@ class _MyHomePageState extends State<MyHomePage> {
   String myMugObj = 'assets/my3dMug5.obj';
 
   /// new jitaku models
-  String cup1 = 'assets/jitaku/base.obj';
+  String cup1 = 'assets/jitaku/Nomal_blue.obj';
   String lid1 = 'assets/jitaku/lid.obj';
   String canister = 'assets/jitaku/Canister.obj';
   String heartMug1 = 'assets/jitaku/heartMug2.1.obj';
@@ -78,7 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   void initState() {
     super.initState();
-    srcObj = 'assets/jitaku/Heart_Boolean.obj';
+    srcObj = cup1;//'assets/latestObjfile/Normal_blue_obj.obj';
     controller.onModelLoaded.addListener(() {
       debugPrint('model is loaded : ${controller.onModelLoaded.value}');
     });
@@ -281,54 +277,41 @@ class _MyHomePageState extends State<MyHomePage> {
                     ),
                   ),
                   /// glb file rendered
-                  Flexible(
-                    flex: 1,
-                    child: Flutter3DViewer(
-                      //If you pass 'true' the flutter_3d_controller will add gesture interceptor layer
-                      //to prevent gesture recognizers from malfunctioning on iOS and some Android devices.
-                      // the default value is true.
-                      activeGestureInterceptor: true,
-                      //If you don't pass progressBarColor, the color of defaultLoadingProgressBar will be grey.
-                      //You can set your custom color or use [Colors.transparent] for hiding loadingProgressBar.
-                      progressBarColor: Colors.orange,
-                      //You can disable viewer touch response by setting 'enableTouch' to 'false'
-                      enableTouch: true,
-                      //This callBack will return the loading progress value between 0 and 1.0
-                      onProgress: (double progressValue) {
-                        debugPrint('model loading progress : $progressValue');
-                      },
-                      //This callBack will call after model loaded successfully and will return model address
-                      onLoad: (String modelAddress) {
-                        debugPrint('model loaded : $modelAddress');
-                        controller.playAnimation();
-                      },
-                      //this callBack will call when model failed to load and will return failure error
-                      onError: (String error) {
-                        debugPrint('model failed to load : $error');
-                      },
-                      //You can have full control of 3d model animations, textures and camera
-                      controller: controller,
-                      src: srcGlb,
-                      // src: 'assets/business_man.glb', //3D model with different animations
-                      //src: 'assets/sheen_chair.glb', //3D model with different textures
-                      //src: 'https://modelviewer.dev/shared-assets/models/Astronaut.glb', // 3D model from URL
-                    ),
-                  ),
+                  // Flexible(
+                  //   flex: 1,
+                  //   child: Flutter3DViewer(
+                  //     //If you pass 'true' the flutter_3d_controller will add gesture interceptor layer
+                  //     //to prevent gesture recognizers from malfunctioning on iOS and some Android devices.
+                  //     // the default value is true.
+                  //     activeGestureInterceptor: true,
+                  //     //If you don't pass progressBarColor, the color of defaultLoadingProgressBar will be grey.
+                  //     //You can set your custom color or use [Colors.transparent] for hiding loadingProgressBar.
+                  //     progressBarColor: Colors.orange,
+                  //     //You can disable viewer touch response by setting 'enableTouch' to 'false'
+                  //     enableTouch: true,
+                  //     //This callBack will return the loading progress value between 0 and 1.0
+                  //     onProgress: (double progressValue) {
+                  //       debugPrint('model loading progress : $progressValue');
+                  //     },
+                  //     //This callBack will call after model loaded successfully and will return model address
+                  //     onLoad: (String modelAddress) {
+                  //       debugPrint('model loaded : $modelAddress');
+                  //       controller.playAnimation();
+                  //     },
+                  //     //this callBack will call when model failed to load and will return failure error
+                  //     onError: (String error) {
+                  //       debugPrint('model failed to load : $error');
+                  //     },
+                  //     //You can have full control of 3d model animations, textures and camera
+                  //     controller: controller,
+                  //     src: srcGlb,
+                  //     // src: 'assets/business_man.glb', //3D model with different animations
+                  //     //src: 'assets/sheen_chair.glb', //3D model with different textures
+                  //     //src: 'https://modelviewer.dev/shared-assets/models/Astronaut.glb', // 3D model from URL
+                  //   ),
+                  // ),
                 ],
               ),
-            ),
-          ),
-
-          /// model viewer flutter package example code
-          Flexible(
-            flex: 1,
-            child: ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => ModelViewerClass()),
-                );
-              },
-              child: Text('model viewer package screen'),
             ),
           ),
           // Flexible(
